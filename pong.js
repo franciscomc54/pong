8aa98636d3951aeeef6d21407bc9da81af5b5dd2
franciscomc54 <fmc54@alu.ua.es>
2022-05-23 18:20:47 +0200

    Canvas Helpers

--- pong.js
+++ pong.js
@@ -2,15 +2,32 @@
 const cvs = document.getElementById('pong_canvas');
 const ctx = cvs.getContext('2d');
 
-ctx.fillStyle = 'BLACK';
-ctx.fillRect(0, 0, 600, 400);
+//Declaraciones
+const FONT_COLOR = 'WHITE';
+const FONT_SIZE = '45px';
+const FONT_FAMILY = 'Impact';
 
-ctx.fillStyle = 'WHITE';
-ctx.beginPath();
-ctx.arc(50, 60, 10, 0, 2*Math.PI);
-ctx.closePath();
-ctx.fill();
 
-ctx.fillStyle = 'BLUE';
-ctx.font = '45px impact';
-ctx.fillText('Saludos', 200, 200);
+//Canvas Helpers -------------------------------------
+function drawRect(x, y, w, h, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, w, h);
+}
+
+function drawCircle(x, y, r, color) {
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.arc(x, y, r, 0, 2*Math.PI);
+    ctx.closePath();
+    ctx.fill();
+}
+
+function drawText(text, x, y, color=FONT_COLOR, fontSize=FONT_SIZE, fontFamily=FONT_FAMILY) {
+    ctx.fillStyle = color;
+    ctx.font = `${fontSize} ${fontFamily}`;
+    ctx.fillText(text, x, y);
+}
+
+drawRect(0, 0, cvs.width, cvs.height, 'BLACK');
+drawCircle(100, 90, 10, 'WHITE');
+drawText('Saludos!!', 200, 200);
